77061d6e8c369942c3e3080cf77f335828b69c69
zoeludena <88351041+zoeludena@users.noreply.github.com>
2021-08-25 21:02:39 +0000

    Akshat helped us with our code and we think this repo will work properly now. Machine learning is cool, but honestly, very complex.

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-#Part 2
-#step 2:
 import pandas as pd
 import nltk
 nltk.download('punkt')
@@ -23,16 +20,12 @@ def get_reviews(data, rating):
     rows = data['Rating']==rating
     return list(data.loc[rows, 'Review'])
 
-#Trying to split up the data in a list based off of a number 0-1
-#So 0.25 will split around 1/4, 0.6 will split it around 1/2
+
 def split_train_test(data, train_prop):
-    ''' input: A list of data, train_prop is a number between 0 and 1
-              specifying the proportion of data in the training set.
-        output: A tuple of two lists, (training, testing)
-    '''
     #The casting function int() and list slicing can make this function just one line:
     #data[0:int(len(data)*train_prop)]
     #Cast is changing the data type of the operation -> string of 1 in the int function, makes it an integer 1 - can multiply integers, but not strings
+    #Float cannot be a list index
     #data.slice(0, -1, int(train_prop))
       #Train_prop is the 0-1 number
     #Use the index of the list to correspond to the train proportion
@@ -48,4 +41,69 @@ print(split_train_test(["A", "B", "C", "D"], 0.55))
 #Convert place you want to split the list into an integer
 #Passes through - you want to find corresponding index in the data, where the 0/25 cut off would be
 
+def format_for_classifier(data_list, label):
+  #Colon separates key in dictionary from value
 
+  #if word in tokens
+
+  return [[format_sentence(document),label] for document in data_list]
+  #Akshat helped us >:D
+  #In the brackets is what you are returning
+    #Run a for loop through data list, then return the format sentence to the document
+    #This is a list: [format_sentence(document),label] -> to get the list for each element need to use the for loop
+print(format_for_classifier(["A good one", "The best!"], "pos"))
+
+#print(format_for_classifier(["Today is a good day!", "Pygame is fun"], "pos"))
+
+def classify_reviews():
+    ''' Perform sentiment classification on movie reviews ''' 
+    # Read the data from the file
+    data = pd.read_csv("data/movie_reviews.csv")
+
+    # get the text of the positive and negative reviews only.
+    # positive and negative will be lists of strings
+    # For now we use only very positive and very negative reviews.
+    positive = get_reviews(data, 4)
+    negative = get_reviews(data, 0)
+
+    # Split each data set into training and testing sets.
+    # You have to write the function split_train_test
+    (pos_train_text, pos_test_text) = split_train_test(positive, 0.8)
+    (neg_train_text, neg_test_text) = split_train_test(negative, 0.8)
+
+    # Format the data to be passed to the classifier.
+    # You have to write the format_for_classifier function
+    pos_train = format_for_classifier(pos_train_text, 'pos')
+    neg_train = format_for_classifier(neg_train_text, 'neg')
+
+    # Create the training set by appending the pos and neg training examples
+    training = pos_train + neg_train
+
+    # Format the testing data for use with the classifier
+    pos_test = format_for_classifier(pos_test_text, 'pos')
+    neg_test = format_for_classifier(neg_test_text, 'neg')
+    # Create the test set
+    test = pos_test + neg_test
+
+
+    # Train a Naive Bayes Classifier
+    # Uncomment the next line once the code above is working
+    classifier = NaiveBayesClassifier.train(training)
+
+    # Uncomment the next two lines once everything above is working
+    print("Accuracy of the classifier is: " + str(accuracy(classifier, test)))
+    classifier.show_most_informative_features()
+
+    # TODO: Calculate and print the accuracy on the positive and negative
+    # documents separately
+    # You will want to use the function classifier.classify, which takes
+    # a document formatted for the classifier and returns the classification
+    # of that document ("pos" or "neg").  For example:
+    # classifier.classify(format_sentence("I love this movie. It was great!"))
+    # will (hopefully!) return "pos"
+
+    # TODO: Print the misclassified examples
+
+
+if __name__ == "__main__":
+    classify_reviews()
